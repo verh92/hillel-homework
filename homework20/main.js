2a33823e4fb9a0712648cc3b6472c7d30e753cf2
verh92 <vverh92@gmail.com>
2021-04-26 20:10:31 +0400

    hw20

--- homework20/main.js
+++ homework20/main.js
@@ -1,8 +1,8 @@
 function factFunc(n) {
-    let res = 1;
-    for (i = 1; i <= n; i++ ){
+    let res = 1n;
+    for (i = 1n; i <= n; i++ ){
         res = res * i;
     } return res;
 }
 
-console.log(factFunc(5n));
+console.log(factFunc(15));
